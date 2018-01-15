fe0f4ef94e2cbf4d519e0cebba44ed5883749d66
Julian Eggers <julian-eggers@users.noreply.github.com>
2018-01-15 17:35:50 +0100

    Script for replica backups added

---
+++ docker-swarm-replica-backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+####
+## Execute "curl -sSL https://raw.githubusercontent.com/avides/docker-scripts/master/docker-swarm-replica-backup.sh | bash"
+####
+
+
+docker service ls | while IFS= read -r line ; do
+
+	## Parse service information
+	name=$(echo $line | awk '{print $2;}' )
+	replicas=$(echo $line | awk '{print $4}');
+
+	## Ignore first line
+	if [ "$name" == "NAME" ]; then continue; fi
+
+	## Parse replica information
+	replicasExpected=$(echo $replicas | awk -F '/' '{print $2}');
+
+	## Print current state
+	echo "$name $replicasExpected"
+
+done
