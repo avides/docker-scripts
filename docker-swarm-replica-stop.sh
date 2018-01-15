431159597b163c70d0f03d7a98615fb7ab5e8f2c
Julian Eggers <julian-eggers@users.noreply.github.com>
2018-01-15 18:07:32 +0100

    Update docker-swarm-replica-stop.sh

--- docker-swarm-replica-stop.sh
+++ docker-swarm-replica-stop.sh
@@ -1,1 +1,15 @@
-swarm-services-start-stop
+#!/bin/bash
+
+####
+## Execute "curl -sSL https://raw.githubusercontent.com/avides/docker-scripts/master/docker-swarm-replica-start.sh | bash"
+####
+
+
+input=`cat`
+
+for serviceReplicas in $input
+do
+        echo "docker service scale $serviceReplicas"
+        docker service scale $serviceReplicas
+        echo ""
+done
