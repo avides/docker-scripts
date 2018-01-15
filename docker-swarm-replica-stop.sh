71ced72acc3d7bd5e053af8d10837de8a4144ba7
Julian Eggers <julian-eggers@users.noreply.github.com>
2018-01-15 18:18:38 +0100

    Create docker-swarm-replica-stop.sh

---
+++ docker-swarm-replica-stop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+####
+## Execute "curl -sSL https://raw.githubusercontent.com/avides/docker-scripts/master/docker-swarm-replica-stop.sh | bash"
+####
+
+
+docker service ls | while IFS= read -r line ; do
+
+	## Parse service information
+	name=$(echo $line | awk '{print $2;}' )
+
+	## Ignore first line
+	if [ "$name" == "NAME" ]; then continue; fi
+
+	## Stop all replicas
+	echo "docker service scale $name=0"
+	docker service scale $name=0
+	echo ""
+done
