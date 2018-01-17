a08fe8f8510fdcfdcd49cc11c4de76d19c2c8a14
Julian Eggers <julian-eggers@users.noreply.github.com>
2018-01-17 11:57:16 +0100

    Update docker-swarm-replica-start.sh

--- docker-swarm-replica-start.sh
+++ docker-swarm-replica-start.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
 ####
-## Execute "curl -sSL https://raw.githubusercontent.com/avides/docker-scripts/master/docker-swarm-replica-start.sh | bash"
+##
+## Scale all services to 1
+##      curl -sSL https://raw.githubusercontent.com/avides/docker-scripts/master/docker-swarm-replica-start.sh | bash
+##
+## Scale all services to previous number
+##      curl -sSL https://raw.githubusercontent.com/avides/docker-scripts/master/docker-swarm-replica-start.sh | bash
+##
 ####
 
 
 ## Read from stdin
 input=`cat`
 
-## Iterate over each service-line
-for serviceReplicas in $input
-do
-        ## Scale replica
-        echo "docker service scale $serviceReplicas"
-        docker service scale $serviceReplicas
-        echo ""
-done
+if [ -z "$VAR" ];
+
+        echo "no stdin"
+
+else
+
+        ## Iterate over each service-line
+        for serviceReplicas in $input
+        do
+                ## Scale replica
+                echo "docker service scale $serviceReplicas"
+                docker service scale $serviceReplicas
+                echo ""
+        done
+
+fi
+
+
